3f01155540422bb0d3fc8c0cada4a27e6eb6ddd3
zey <zeynadieng10@gmail.com>
2026-03-05 08:58:12 +0000

    feat: add listProducts() - v0.1.0-SNAPSHOT

--- src/main/java/sn/isi/l3gl/core/service/ProductService.java
+++ src/main/java/sn/isi/l3gl/core/service/ProductService.java
@@ -3,6 +3,7 @@ package sn.isi.l3gl.core.service;
 import org.springframework.stereotype.Service;
 import sn.isi.l3gl.core.entity.Product;
 import sn.isi.l3gl.core.repository.ProductRepository;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -13,7 +14,13 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    // v0.0.1-SNAPSHOT
     public Product createProduct(Product product) {
         return productRepository.save(product);
+    }
+
+    // v0.1.0-SNAPSHOT
+    public List<Product> listProducts() {
+        return productRepository.findAll();
     }
 }
